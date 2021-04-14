07bf89a160eea1853bc49ac24846a2e6e792ecdd
jjiyeon <ejy1105@gmail.com>
2021-04-14 21:11:40 +0900

    level1 : 나누어 떨어지는 숫자 배열 다른사람 풀이 추가!

--- level1/divisor.js
+++ level1/divisor.js
@@ -18,3 +18,13 @@ function solution(arr, divisor) {
 */
 const arr = [3, 2, 6];
 console.log(solution(arr, 10));
+
+/*
+  다른 사람 풀이!
+  이렇게 간결하게 사용하는 연습을 하자!
+  filter를 사용하면서 아래를 꿈꿨는데
+ */
+function solution(arr, divisor) {
+  var answer = arr.filter(v => v % divisor == 0);
+  return answer.length == 0 ? [-1] : answer.sort((a, b) => a - b);
+}
