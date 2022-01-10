4a25fb0d23ea937da167561ebfa8c93d3f599d4a
jjiyeon <ejy1105@gmail.com>
2022-01-10 21:44:05 +0900

    feat: stack 강의 풀이 추가

--- stack/stack-01.js
+++ stack/stack-01.js
@@ -13,6 +13,7 @@
  * 
  */
 
+// 나의 풀이
 function solution(s) {
   if (s.length % 2 !== 0) return false;
 
@@ -32,4 +33,22 @@ function solution(s) {
   } else {
     return false;
   }
+}
+
+// 강의 풀이
+function solution(s) {
+  let count = 0;
+
+  for (const c of s) {
+    if (c === "(") {
+      count += 1;
+    } else {
+      if (count === 0) {
+        return false;
+      }
+      count -= 1;
+    }
+  }
+
+  return count === 0;
 }
