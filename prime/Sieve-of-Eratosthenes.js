18bc592541205de41eb145310f1e24cc35efbe76
Journey <ejy1105@gmail.com>
2022-04-11 01:17:09 +0900

    feat: 에라토스테네스의 체 - 소수구하기

---
+++ prime/Sieve-of-Eratosthenes.js
@@ -0,0 +1,13 @@
+//1부터 N까지 소수를 구할때 에라토스테네스의 체가 효율적이다.
+function get_primes(num) {
+  const prime = [false, false, ...Array(num - 1).fill(true)];
+
+  for (let i = 2; i * i <= num; i += 1) {
+    if (prime[i]) {
+      for (let j = i * 2; j <= num; j += i) {
+        prime[j] = false;
+      }
+    }
+  }
+  return prime.filter(Boolean).length;
+}
